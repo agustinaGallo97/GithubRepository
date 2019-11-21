dd90b20a4f42e63c874381a64b6339e659e40639
Agustina <agustina@xmartlabs.com>
2019-11-21 16:31:32 -0300

    Finish chapter 1

--- GeoQuizChallenge/app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ GeoQuizChallenge/app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -1,12 +1,17 @@
 package com.bignerdranch.android.geoquiz;
-import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.view.Gravity;
 import android.widget.Button;
 import android.widget.Toast;
-import android.view.Gravity;
+
+import androidx.annotation.StringRes;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class QuizActivity extends AppCompatActivity {
+  @SuppressWarnings("FieldCanBeLocal")
   private Button trueButton;
+  @SuppressWarnings("FieldCanBeLocal")
   private Button falseButton;
 
   @Override
@@ -15,23 +20,16 @@ public class QuizActivity extends AppCompatActivity {
     setContentView(R.layout.activity_quiz);
 
     trueButton = findViewById(R.id.question_button_true);
-    trueButton.setOnClickListener(v -> {
-      showToastCorrectly(true);
-    });
+    trueButton.setOnClickListener(v -> showToastCorrectly(true));
 
     falseButton = findViewById(R.id.question_button_false);
-    falseButton.setOnClickListener(v -> {
-      showToastCorrectly(false);
-    });
+    falseButton.setOnClickListener(v -> showToastCorrectly(false));
   }
 
   private void showToastCorrectly(boolean isCorrect) {
-    Toast toast;
-    if (isCorrect) {
-      toast = Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT);
-    } else {
-      toast = Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT);
-    }
+    @StringRes
+    int answerTest = isCorrect ? R.string.correct_toast : R.string.incorrect_toast;
+    Toast toast = Toast.makeText(this, answerTest, Toast.LENGTH_SHORT);
     toast.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL, 0, 0);
     toast.show();
   }
