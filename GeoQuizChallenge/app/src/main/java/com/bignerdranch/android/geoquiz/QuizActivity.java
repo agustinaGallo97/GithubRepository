06e638ba93b322de0e8b48effa1baea0d49d4a03
Agustina <agustina@xmartlabs.com>
2019-11-21 15:05:12 -0300

    Adding suggestions

--- GeoQuizChallenge/app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ GeoQuizChallenge/app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -1,42 +1,39 @@
 package com.bignerdranch.android.geoquiz;
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 import android.view.Gravity;
 
-
 public class QuizActivity extends AppCompatActivity {
-  private Button mTrueButton;
-  private Button mFalseButton;
+  private Button trueButton;
+  private Button falseButton;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_quiz);
 
-    mTrueButton = (Button) findViewById(R.id.question_button_true);
-    mTrueButton.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        Toast toast = Toast.makeText(QuizActivity.this, R.string.coorect_toast, Toast.LENGTH_SHORT);
-        toast.setGravity(Gravity.TOP|Gravity.CENTER_HORIZONTAL, 0, 0);
-        toast.show();
-      }
+    trueButton = findViewById(R.id.question_button_true);
+    trueButton.setOnClickListener(v -> {
+      ShowToastCorrectly(true);
     });
 
-    mFalseButton = (Button) findViewById(R.id.question_button_false);
-    mFalseButton.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        Toast toast = Toast.makeText(QuizActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT);
-        toast.setGravity(Gravity.TOP|Gravity.CENTER_HORIZONTAL, 0, 0);
-        toast.show();
-      }
+    falseButton = findViewById(R.id.question_button_false);
+    falseButton.setOnClickListener(v -> {
+      ShowToastCorrectly(false);
     });
   }
+
+  private void ShowToastCorrectly(boolean isCorrect) {
+    Toast toast;
+    if (isCorrect) {
+      toast = Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT);
+    } else {
+      toast = Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT);
+    }
+    toast.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL, 0, 0);
+    toast.show();
+  }
 }
+
