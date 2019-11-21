40eb08cff5bb4d14100c2b9ab7c07957d5abab62
Agustina <agustina@xmartlabs.com>
2019-11-21 17:20:44 -0300

    Chapter 2 init

---
+++ GeoQuizChallenge/app/src/main/java/com/bignerdranch/android/geoquiz/Question.java
@@ -0,0 +1,27 @@
+package com.bignerdranch.android.geoquiz;
+
+public class Question {
+  private int textResId;
+  private boolean answerTrue;
+
+  public Question(int textResId, boolean answerTrue) {
+    this.textResId = textResId;
+    this.answerTrue = answerTrue;
+  }
+
+  public int getTextResId() {
+    return this.textResId;
+  }
+
+  public void setTextResId(int textResId) {
+    this.textResId = textResId;
+  }
+
+  public boolean isAnswerTrue() {
+    return this.answerTrue;
+  }
+
+  public void setAnswerTrue(boolean answerTrue) {
+    this.answerTrue = answerTrue;
+  }
+}
--- GeoQuizChallenge/app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ GeoQuizChallenge/app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -1,29 +1,72 @@
 package com.bignerdranch.android.geoquiz;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.StringRes;
-import androidx.appcompat.app.AppCompatActivity;
 
 public class QuizActivity extends AppCompatActivity {
   @SuppressWarnings("FieldCanBeLocal")
   private Button trueButton;
   @SuppressWarnings("FieldCanBeLocal")
   private Button falseButton;
+  private ImageButton nextButton;
+  private ImageButton prevButton;
+  private TextView questionTextView;
+  private Question[] questionBank = new Question[] {
+      new Question(R.string.question_australia, true),
+      new Question(R.string.question_oceans, true),
+      new Question(R.string.question_mideast, true),
+      new Question(R.string.question_africa, true),
+      new Question(R.string.question_americas, true),
+      new Question(R.string.question_asia, true),
+  };
+  private int currentIndex = 0;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_quiz);
 
+    questionTextView = findViewById(R.id.question_text_view);
+    questionTextView.setOnClickListener(v -> {
+      currentIndex = (currentIndex + 1) % questionBank.length;
+      updateQuestion();
+    });
+
     trueButton = findViewById(R.id.question_button_true);
     trueButton.setOnClickListener(v -> showToastCorrectly(true));
 
     falseButton = findViewById(R.id.question_button_false);
     falseButton.setOnClickListener(v -> showToastCorrectly(false));
+
+    prevButton = findViewById(R.id.prev_button);
+    prevButton.setOnClickListener(v -> {
+      if (currentIndex > 0) {
+        currentIndex = (currentIndex - 1) % questionBank.length;
+        updateQuestion();
+      }
+    });
+
+    nextButton = (ImageButton) findViewById(R.id.next_button);
+    nextButton.setOnClickListener(v -> {
+      currentIndex = (currentIndex + 1) % questionBank.length;
+      updateQuestion();
+    });
+
+    updateQuestion();
+  }
+
+  private void updateQuestion() {
+    int question = questionBank[currentIndex].getTextResId();
+    questionTextView.setText(question);
   }
 
   private void showToastCorrectly(boolean isCorrect) {
