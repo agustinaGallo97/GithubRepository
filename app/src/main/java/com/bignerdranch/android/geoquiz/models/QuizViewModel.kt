bf0416a194ffc84f8f13e06dc99684b0355a09b4
Agustina <33355750+agustinaGallo97@users.noreply.github.com>
2019-11-29 14:10:45 -0300

    chapter 4 android-kotlin created (#8)

---
+++ app/src/main/java/com/bignerdranch/android/geoquiz/models/QuizViewModel.kt
@@ -0,0 +1,26 @@
+package com.bignerdranch.android.geoquiz.models
+
+import androidx.lifecycle.ViewModel
+import com.bignerdranch.android.geoquiz.controllers.QuestionController
+
+class QuizViewModel : ViewModel() {
+    var currentIndex = 0
+    val questionBank = QuestionController.getInstance().questionBank
+
+    val currentQuestion: Question
+        get() = questionBank[currentIndex]
+
+    val currentQuestionAnswer: Boolean
+        get() = questionBank[currentIndex].isAnswerTrue
+
+    val currentQuestionText: Int
+        get() = questionBank[currentIndex].textResId
+
+    fun moveToNext() {
+        currentIndex = (currentIndex + 1) % questionBank.size
+    }
+
+    fun moveToPrev() {
+        currentIndex = (currentIndex - 1) % questionBank.size
+    }
+}
